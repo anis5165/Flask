0508efe06fa4fcc8d1d5cfc7c0dbbe544b33aca3
anis5165 <anishkumargvm02@gmail.com>
2025-10-15 13:31:37 +0530

    Implement feedback form and thank you page; update home, login, and profile templates

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def student_profile():
+def home():
+    return render_template('home.html')
+
+@app.route('/feedback', methods=['GET','POST'])
+def feedback():
+    if request.method == 'POST':
+        name = request.form.get('username') 
+        message = request.form.get('message')
+
+        return render_template('thankyou.html', user=name, message=message)
+    
+    return render_template("feedback.html")
+
+
+
+
+@app.route('/profile')
+def profile():
     return render_template(
         "profile.html",
         name="Arun",
         is_toppe=True,
         subjects = ['Maths','Science','History']
     )
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
